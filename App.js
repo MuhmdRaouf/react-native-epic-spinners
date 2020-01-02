71bad1ec179b33348785d4c3b4eb1484b16658f9
Mohammed Abdel Ra'ouf <mohammed.raouf.m@gmail.com>
2020-01-02 11:42:26 +0200

    port 6th spinner [AtomSpinner]

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { IntersectingCirclesSpinner } from './src/components/IntersectingCirclesSpinner';
+import { AtomSpinner } from './src/components/AtomSpinner';
 
 const App: () => React$Node = () => {
   const style = StyleSheet.create({
@@ -16,7 +16,7 @@ const App: () => React$Node = () => {
   });
   return (
     <View style={style.container}>
-      <IntersectingCirclesSpinner />
+      <AtomSpinner />
     </View>
   );
 };
--- src/components/AtomSpinner.js
+++ src/components/AtomSpinner.js
@@ -1,108 +1,142 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-const Atom = styled.div`
-  height: ${(props) => props.size}px;
-  width: ${(props) => props.size}px;
-  overflow: hidden;
-
-  * {
-    box-sizing: border-box;
-  }
-
-  .spinner-inner {
-    position: relative;
-    display: block;
-    height: 100%;
-    width: 100%;
-  }
-
-  .spinner-circle {
-    display: block;
-    position: absolute;
-    color: ${(props) => props.color};
-    font-size: calc(${(props) => props.size}px * 0.24);
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-
-  .spinner-line {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    animation-duration: ${(props) => props.animationDuration}ms;
-    border-left-width: calc(${(props) => props.size}px / 25);
-    border-top-width: calc(${(props) => props.size}px / 25);
-    border-left-color: ${(props) => props.color};
-    border-left-style: solid;
-    border-top-style: solid;
-    border-top-color: transparent;
-  }
-
-  .spinner-line:nth-child(1) {
-    animation: atom-spinner-animation-1 ${(props) => props.animationDuration}ms linear infinite;
-    transform: rotateZ(120deg) rotateX(66deg) rotateZ(0deg);
-  }
-
-  .spinner-line:nth-child(2) {
-    animation: atom-spinner-animation-2 ${(props) => props.animationDuration}ms linear infinite;
-    transform: rotateZ(240deg) rotateX(66deg) rotateZ(0deg);
-  }
-
-  .spinner-line:nth-child(3) {
-    animation: atom-spinner-animation-3 ${(props) => props.animationDuration}ms linear infinite;
-    transform: rotateZ(360deg) rotateX(66deg) rotateZ(0deg);
-  }
-
-  @keyframes atom-spinner-animation-1 {
-    100% {
-      transform: rotateZ(120deg) rotateX(66deg) rotateZ(360deg);
-    }
-  }
-  @keyframes atom-spinner-animation-2 {
-    100% {
-      transform: rotateZ(240deg) rotateX(66deg) rotateZ(360deg);
-    }
-  }
-  @keyframes atom-spinner-animation-3 {
-    100% {
-      transform: rotateZ(360deg) rotateX(66deg) rotateZ(360deg);
-    }
-  }
-`;
-
-const propTypes = {
-  size: PropTypes.number,
-  animationDuration: PropTypes.number,
-  color: PropTypes.string,
-  style: PropTypes.object
-};
+/** @flow **/
+import type { Element } from 'react';
+import React, { useEffect, useState } from 'react';
+import type { ViewStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
+import { Animated, Easing, StyleSheet, View } from 'react-native';
 
 type EpicProps = {
   size?: number,
   animationDuration?: number,
   color?: string,
-  style?: object
+  style?: ViewStyleProp
+};
+const EpicSpinnersDefaultProps = {
+  size: 100,
+  color: 'red',
+  animationDuration: 1000
 };
 
-export const AtomSpinner = ({ size, animationDuration, color, style, ...props }: EpicProps) => {
-  const x = 10;
+export const AtomSpinner = (props: EpicProps): Element<any> => {
+  const { size, animationDuration, color, style } = props;
+  const [firstSpinnerLine] = useState(new Animated.Value(0));
+  const [secondSpinnerLine] = useState(new Animated.Value(0));
+  const [thirdSpinnerLine] = useState(new Animated.Value(0));
+
+  const spinnerStyle = StyleSheet.create({
+    container: {
+      height: size,
+      width: size,
+      overflow: 'hidden'
+    },
+    spinnerInner: {
+      position: 'relative',
+      height: '100%',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    spinnerCircle: {
+      height: size * 0.18,
+      width: size * 0.18,
+      backgroundColor: color,
+      borderRadius: size * 0.5
+    },
+    spinnerLine: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      borderRadius: size * 0.5,
+      borderLeftWidth: size / 25,
+      borderTopWidth: size / 25,
+      borderLeftColor: color,
+      borderTopColor: 'transparent'
+    }
+  });
+  const animateStyle = {
+    firstSpinnerLine: {
+      transform: [
+        {
+          rotateZ: '120deg'
+        },
+        {
+          rotateX: '66deg'
+        },
+        {
+          rotateZ: firstSpinnerLine.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+          })
+        }
+      ]
+    },
+    secondSpinnerLine: {
+      transform: [
+        {
+          rotateZ: '240deg'
+        },
+        {
+          rotateX: '66deg'
+        },
+        {
+          rotateZ: firstSpinnerLine.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+          })
+        }
+      ]
+    },
+    thirdSpinnerLine: {
+      transform: [
+        {
+          rotateZ: '360deg'
+        },
+        {
+          rotateX: '66deg'
+        },
+        {
+          rotateZ: firstSpinnerLine.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+          })
+        }
+      ]
+    }
+  };
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.parallel([
+        Animated.timing(firstSpinnerLine, {
+          toValue: 1,
+          duration: animationDuration,
+          easing: Easing.linear
+        }),
+        Animated.timing(secondSpinnerLine, {
+          toValue: 1,
+          duration: animationDuration,
+          easing: Easing.linear
+        }),
+        Animated.timing(thirdSpinnerLine, {
+          toValue: 1,
+          duration: animationDuration,
+          easing: Easing.linear
+        })
+      ])
+    ).start();
+  }, [animationDuration, firstSpinnerLine, secondSpinnerLine, thirdSpinnerLine]);
+
   return (
-    <Atom size={size} color={color} style={style} {...props}>
-      <div className='spinner-inner'>
-        <div className='spinner-line' />
-        <div className='spinner-line' />
-        <div className='spinner-line' />
-        <div className='spinner-circle' />
-      </div>
-    </Atom>
+    <View style={style} {...props}>
+      <View style={spinnerStyle.container}>
+        <View style={[spinnerStyle.spinnerInner]}>
+          <Animated.View style={[spinnerStyle.spinnerLine, animateStyle.firstSpinnerLine]} />
+          <Animated.View style={[spinnerStyle.spinnerLine, animateStyle.secondSpinnerLine]} />
+          <Animated.View style={[spinnerStyle.spinnerLine, animateStyle.thirdSpinnerLine]} />
+          <View style={[spinnerStyle.spinnerCircle]} />
+        </View>
+      </View>
+    </View>
   );
 };
 
-AtomSpinner.propTypes = propTypes;
-AtomSpinner.defaultProps = defaultProps;
-
-export default AtomSpinner;
+AtomSpinner.defaultProps = EpicSpinnersDefaultProps;
