9f3400a9c0147633d2bc3d8b83e37834996db717
Mohammed Abdel Ra'ouf <mohammed.raouf.m@gmail.com>
2020-01-02 19:39:31 +0200

    port 9th spinner [FulfillingSquareSpinner]

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { FlowerSpinner } from './src/components/FlowerSpinner';
+import { FulfillingSquareSpinner } from './src/components/FulfillingSquareSpinner';
 
 const App: () => React$Node = () => {
   const style = StyleSheet.create({
@@ -16,7 +16,7 @@ const App: () => React$Node = () => {
   });
   return (
     <View style={style.container}>
-      <FlowerSpinner />
+      <FulfillingSquareSpinner />
     </View>
   );
 };
--- src/components/FulfillingSquareSpinner.js
+++ src/components/FulfillingSquareSpinner.js
@@ -1,102 +1,82 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+/** @flow **/
+import type { Element } from 'react';
+import React, { useEffect, useState } from 'react';
+import type { ViewStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
+import { Animated, Easing, StyleSheet, View } from 'react-native';
 
-const SquareSpinner = styled.div`
-  height: ${props => props.size}px;
-  width: ${props => props.size}px;
-  position: relative;
-  border: 4px solid ${props => props.color};
-  animation: fulfilling-square-spinner-animation
-    ${props => props.animationDuration}ms infinite ease;
-
-  * {
-    box-sizing: border-box;
-  }
-
-  .spinner-inner {
-    vertical-align: top;
-    display: inline-block;
-    background-color: ${props => props.color};
-    width: 100%;
-    opacity: 1;
-    animation: fulfilling-square-spinner-inner-animation
-      ${props => props.animationDuration}ms infinite ease-in;
-  }
-
-  @keyframes fulfilling-square-spinner-animation {
-    0% {
-      transform: rotate(0deg);
-    }
-    25% {
-      transform: rotate(180deg);
-    }
-    50% {
-      transform: rotate(180deg);
-    }
-    75% {
-      transform: rotate(360deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-
-  @keyframes fulfilling-square-spinner-inner-animation {
-    0% {
-      height: 0%;
-    }
-    25% {
-      height: 0%;
-    }
-    50% {
-      height: 100%;
-    }
-    75% {
-      height: 100%;
-    }
-    100% {
-      height: 0%;
-    }
-  }
-`;
-
-const propTypes = {
-  size: PropTypes.number,
-  animationDuration: PropTypes.number,
-  color: PropTypes.string,
-  className: PropTypes.string,
-  style: PropTypes.object,
+type EpicProps = {
+  size?: number,
+  animationDuration?: number,
+  color?: string,
+  style?: ViewStyleProp
 };
-
-const defaultProps = {
+const EpicSpinnersDefaultProps = {
   size: 50,
-  color: '#fff',
-  animationDuration: 4000,
-  className: '',
+  color: 'red',
+  animationDuration: 4000
 };
 
-const FulfillingSquareSpinner = ({
-  size,
-  color,
-  animationDuration,
-  className,
-  style,
-  ...props
-}) => (
-  <SquareSpinner
-    size={size}
-    color={color}
-    animationDuration={animationDuration}
-    className={`fulfilling-square-spinner${className ? ' ' + className : ''}`}
-    style={style}
-    {...props}
-  >
-    <div className="spinner-inner" />
-  </SquareSpinner>
-);
+export const FulfillingSquareSpinner = (props: EpicProps): Element<any> => {
+  const { size, animationDuration, color, style } = props;
+  const [animatedContainer] = useState(new Animated.Value(0));
+  const [animatedInnerSpinner] = useState(new Animated.Value(0));
+  const spinnerStyle = StyleSheet.create({
+    container: {
+      height: size,
+      width: size,
+      position: 'relative',
+      borderWidth: size * 0.1,
+      borderColor: color
+    },
+    spinnerInner: {
+      backgroundColor: color,
+      width: '100%',
+      opacity: 1
+    }
+  });
+  const animateStyle = {
+    container: {
+      transform: [
+        {
+          rotate: animatedContainer.interpolate({
+            inputRange: [0, 1, 2, 4, 5],
+            outputRange: ['0deg', '180deg', '180deg', '360deg', '360deg']
+          })
+        }
+      ]
+    },
+    spinnerInner: {
+      height: animatedInnerSpinner.interpolate({
+        inputRange: [0, 1, 2, 4, 5],
+        outputRange: ['0%', '0%', '100%', '100%', '0%']
+      })
+    }
+  };
 
-FulfillingSquareSpinner.propTypes = propTypes;
-FulfillingSquareSpinner.defaultProps = defaultProps;
+  useEffect(() => {
+    Animated.loop(
+      Animated.parallel([
+        Animated.timing(animatedContainer, {
+          toValue: 5,
+          duration: animationDuration,
+          easing: Easing.ease
+        }),
+        Animated.timing(animatedInnerSpinner, {
+          toValue: 5,
+          duration: animationDuration,
+          easing: Easing.ease
+        })
+      ])
+    ).start();
+  }, [animatedContainer, animatedInnerSpinner, animationDuration]);
 
-export default FulfillingSquareSpinner;
+  return (
+    <View style={style} {...props}>
+      <Animated.View style={[spinnerStyle.container, animateStyle.container]}>
+        <Animated.View style={[spinnerStyle.spinnerInner, animateStyle.spinnerInner]} />
+      </Animated.View>
+    </View>
+  );
+};
+
+FulfillingSquareSpinner.defaultProps = EpicSpinnersDefaultProps;
